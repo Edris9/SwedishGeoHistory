03e54313d467b152951ac959c88a14ddae6afe11
Edris9 <Edriskohestani1010@gmail.com>
2026-02-02 12:19:27 +0100

    change database from postg to subabase, for free sake

--- backend-api/obj/Debug/net9.0/backend-api.AssemblyInfo.cs
+++ backend-api/obj/Debug/net9.0/backend-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72396ff5821f1a0b295595d8ab580a74891cbd0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842de4d68c88534da006fd5e1a425197dbca1b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
